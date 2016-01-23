befb56ba60ac6f64ce3bac372566752d93ee0ce0
Matthew Gall <cloud9@matthewgall.com>
2016-01-23 10:39:08 +0000

    As per pylint, changing tab indentation to spaces. Que sera sera

--- application.py
+++ application.py
@@ -12,101 +12,101 @@ from bottle import route, request, response, error, default_app, view, static_fi
 from tinydb import TinyDB, where
 
 def mailgunVerify(mailToken, mailTimestamp, mailSignature):
-	return mailSignature == hmac.new(
-		key=mailgunToken,
-		msg='{}{}'.format(mailTimestamp, mailToken),
-		digestmod=hashlib.sha256).hexdigest()
+    return mailSignature == hmac.new(
+        key=mailgunToken,
+        msg='{}{}'.format(mailTimestamp, mailToken),
+        digestmod=hashlib.sha256).hexdigest()
 
 @route('/favicon.ico')
 @route('/import/mailgun', method='GET')
 @error(404)
 def error404():
-	response.status = 404
-	return 'Not Found'
+    response.status = 404
+    return 'Not Found'
 
 @error(403)
 def error403(error):
-	response.status = 403
-	return 'Access forbidden'
-	
+    response.status = 403
+    return 'Access forbidden'
+    
 @route('/version')
 def return_version():
-	try:
-		dirname, filename = os.path.split(os.path.abspath(__file__))
-		del filename
-		f = open(os.getenv('VERSION_PATH', dirname + '/.git/refs/heads/master'), 'r')
-		content = f.read()
-		response.content_type = 'text/plain'
-		return content
-	except:
-		return "Unable to open version file."
-		
+    try:
+        dirname, filename = os.path.split(os.path.abspath(__file__))
+        del filename
+        f = open(os.getenv('VERSION_PATH', dirname + '/.git/refs/heads/master'), 'r')
+        content = f.read()
+        response.content_type = 'text/plain'
+        return content
+    except:
+        return "Unable to open version file."
+        
 @route('/import/mailgun', method="POST")
 def incrementCountMail():
 
-	if mailgunToken == '':
-		log.error('Received call to /import/mailgun with MAILGUN_TOKEN not set. E-mail processing disabled.')
-		raise HTTPError(404)
-	
-	emailAddress = request.forms.get('sender')
-	emailRecipient = request.forms.get('recipient')
-	emailSubject = request.forms.get('subject')
-	
-	emailToken = request.forms.get('token')
-	emailTimestamp = request.forms.get('timestamp')
-	emailSignature = request.forms.get('signature')
-	
-	if not mailgunVerify(emailToken, emailTimestamp, emailSignature):
-		log.info("Discarding e-mail from " + emailAddress, " , signature verification failed")
-		raise HTTPError(403)
-	else:
-		log.info("Accepted e-mail from " + emailAddress + ", (recipient: " + emailRecipient + ")")
-		return "Accepted"
+    if mailgunToken == '':
+        log.error('Received call to /import/mailgun with MAILGUN_TOKEN not set. E-mail processing disabled.')
+        raise HTTPError(404)
+    
+    emailAddress = request.forms.get('sender')
+    emailRecipient = request.forms.get('recipient')
+    emailSubject = request.forms.get('subject')
+    
+    emailToken = request.forms.get('token')
+    emailTimestamp = request.forms.get('timestamp')
+    emailSignature = request.forms.get('signature')
+    
+    if not mailgunVerify(emailToken, emailTimestamp, emailSignature):
+        log.info("Discarding e-mail from " + emailAddress, " , signature verification failed")
+        raise HTTPError(403)
+    else:
+        log.info("Accepted e-mail from " + emailAddress + ", (recipient: " + emailRecipient + ")")
+        return "Accepted"
 
 @route('/count/<id>', method='GET')
 def getCounter(id):
-	return getCounter
-	
+    return getCounter
+    
 @route('/count/<id>', method='POST')
 def incrementCounter(id):
-	return getCounter
+    return getCounter
 
 @route('/')
 def index():
-	return "Hello, world!"
+    return "Hello, world!"
 
 if __name__ == '__main__':
 
-	app = default_app()
+    app = default_app()
 
-	appReload = os.getenv('APP_RELOAD', False)
-	appSecret = os.getenv('APP_SECRET', '')
-	
-	serverHost = os.getenv('SERVER_HOST', 'localhost')
-	serverPort = os.getenv('SERVER_PORT', '5000')
-	
-	mailgunToken = os.getenv('MAILGUN_TOKEN', '')
-	logentriesToken = os.getenv('LOGENTRIES_TOKEN', '')
+    appReload = os.getenv('APP_RELOAD', False)
+    appSecret = os.getenv('APP_SECRET', '')
+    
+    serverHost = os.getenv('SERVER_HOST', 'localhost')
+    serverPort = os.getenv('SERVER_PORT', '5000')
+    
+    mailgunToken = os.getenv('MAILGUN_TOKEN', '')
+    logentriesToken = os.getenv('LOGENTRIES_TOKEN', '')
 
-	# Now we're ready, so start the server
-	# Instantiate the logger
-	log = logging.getLogger('log')
-	console = logging.StreamHandler()
-	log.setLevel(logging.INFO)
-	log.addHandler(console)
+    # Now we're ready, so start the server
+    # Instantiate the logger
+    log = logging.getLogger('log')
+    console = logging.StreamHandler()
+    log.setLevel(logging.INFO)
+    log.addHandler(console)
 
-	if logentriesToken != '':
-		log.addHandler(LogentriesHandler(os.getenv('LOGENTRIES_TOKEN', '')))
+    if logentriesToken != '':
+        log.addHandler(LogentriesHandler(os.getenv('LOGENTRIES_TOKEN', '')))
 
-	if appSecret == '':
-		log.error('Secure tokens disabled, using empty secret. Cookies will not be encrypted. Set APP_SECRET to secure seed and restart.')
-	
-	if mailgunToken == '':
-		log.error('Unable to connect to mailgun API. Incrementing counters via e-mail will be disabled. Set MAILGUN_TOKEN to your domain API key and restart.')
+    if appSecret == '':
+        log.error('Secure tokens disabled, using empty secret. Cookies will not be encrypted. Set APP_SECRET to secure seed and restart.')
+    
+    if mailgunToken == '':
+        log.error('Unable to connect to mailgun API. Incrementing counters via e-mail will be disabled. Set MAILGUN_TOKEN to your domain API key and restart.')
 
-	# Now we're ready, so start the server
-	try:
-		log.info("Successfully started application server on " + socket.gethostname())
-		app.run(host=serverHost, port=serverPort, reloader=bool(appReload))
-	except:
-		log.error("Failed to start application server on " + socket.gethostname())
+    # Now we're ready, so start the server
+    try:
+        log.info("Successfully started application server on " + socket.gethostname())
+        app.run(host=serverHost, port=serverPort, reloader=bool(appReload))
+    except:
+        log.error("Failed to start application server on " + socket.gethostname())
